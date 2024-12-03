10be13ff819e20f4ea9bb07e726256bc779532e8
Sifa Gakeni Muriithi <gakenisifa@gmail.com>
2024-12-03 12:35:05 +0300

    attributes and methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,59 @@
 class Song:
-    pass
+
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls, increment=1):
+        cls.count += increment
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+song1 = Song("me and u", " Tems", "RnB")
+song2 = Song("Happy", " Tems", "Rock")
+song3 = Song("May 10th", " Serotonin", "Afro")
+song4 = Song("Venus", " Serotonin", "Salsa")
+song5 = Song("Swing", "Mal", "Salsa")
+song6 = Song("Ana Mille", "Yarden", "Afro")
+song7 = Song("Que Sera", "MIA", "Afro")
+
+print(Song.count)
+print(Song.genres)
+print(Song.artists)
+print(Song.genre_count)
+print(Song.artist_count)
